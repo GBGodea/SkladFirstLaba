4e77340c873c71addaa09dfdb081108c257a7d47
GBGodea <125504532+GBGodea@users.noreply.github.com>
2025-09-10 15:29:28 +0400

    Starting a project with requirements

---
+++ src/Entities/Items.java
@@ -0,0 +1,9 @@
+package Entities;
+
+/*
+    TODO
+    Сделать entity предмета, который будет храниться на складе
+    id, количество, название, и т.д.
+ */
+public class Items {
+}
---
+++ src/Warehouse.java
@@ -0,0 +1,13 @@
+/*
+    TODO
+    Сделать привязку через DI(самостоятельно написанный DI на reflection)
+    чтобы обрабатывать пользователей, и предметы
+
+    Создать отдельный класс для сборка статистики и собственный коллектор
+ */
+
+public class Warehouse {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
